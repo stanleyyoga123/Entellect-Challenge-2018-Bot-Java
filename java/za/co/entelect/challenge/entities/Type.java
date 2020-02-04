b67371b8b2711b22b53c5e20abe1cdb7f480677f
stanleyyoga123 <yoga360227@gmail.com>
2020-02-04 09:58:02 +0700

    mantap

---
+++ java/za/co/entelect/challenge/entities/Type.java
@@ -0,0 +1,13 @@
+package za.co.entelect.challenge.entities;
+
+public class Type {
+    public int a;
+    public int e;
+    public int d;
+
+    public Type(){
+        this.a = 0;
+        this.e = 0;
+        this.d = 0;
+    }
+}
